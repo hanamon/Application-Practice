e7293a4a6a72b6192c921087127c52867e6a0c51
hanamon <devparkhana@gmail.com>
2021-10-24 22:58:20 +0900

    Add import payment api functions

--- server/app.js
+++ server/app.js
@@ -165,6 +165,71 @@ app.post('/iamport-webhook', async (req, res) => {
   }
 });
 
+// "/payments/complete/mobile/"에 대한 GET 요청을 처리
+app.get('/payments/complete/mobile/', async (req, res) => {
+  try {
+    // req의 query에서 imp_uid, merchant_uid 추출
+    const { imp_uid, merchant_uid } = req.query;
+
+    // 1. 액세스 토큰(access token) 발급 받기
+    const getToken = await axios({
+      url: 'https://api.iamport.kr/users/getToken',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      data: {
+        imp_key: process.env.IMP_KEY, // REST API 키
+        imp_secret: process.env.IMP_SECRET // REST API Secret
+      }
+    });
+    // 인증 토큰 추출하기
+    const { access_token } = getToken.data.response;
+    console.log('access_token', access_token);
+
+    // 2. imp_uid로 아임포트 서버에서 결제 정보 조회
+    const getPaymentData = await axios({
+      url: `https://api.iamport.kr/payments/${imp_uid}`, // imp_uid 전달
+      method: 'GET', // GET method
+      headers: { Authorization: access_token } // 인증 토큰 Authorization header에 추가
+    });
+    // 조회한 결제 정보
+    const paymentData = getPaymentData.data.response;
+    console.log('paymentData', paymentData);
+
+    // 3. DB에서 결제되어야 하는 금액 조회
+    const order = await order.findOne(paymentData.merchant_uid);
+    const amountToBePaid = order.amount; // 결제 되어야 하는 금액
+
+    // 4. 결제 검증하기
+    const { amount, status } = paymentData;
+
+    if (amount === amountToBePaid) {
+      // 결제금액 일치. 결제 된 금액 === 결제 되어야 하는 금액
+      await order.update(
+        // DB에 결제 정보 저장
+        {
+          card_name: paymentData.card_name,
+          status: paymentData.status
+        },
+        { where: { merchant_uid } }
+      );
+      switch (status) {
+        case 'paid': // 결제 완료
+          res.json({ status: 'success', message: '일반 결제 성공' });
+          break;
+      }
+    } else {
+      // 결제금액 불일치. 위/변조 된 결제
+      throw { status: 'forgery', message: '위조된 결제시도' };
+    }
+
+    // 이후 적절한 로직 구현
+    return res.json({ paymentData });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ err, message: 'Client Error' });
+  }
+});
+
 // Error handling
 app.use((req, res, next) => {
   const error = new Error(
